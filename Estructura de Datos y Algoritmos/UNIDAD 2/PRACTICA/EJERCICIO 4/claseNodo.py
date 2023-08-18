ed46b71c779260090d88d7bf239fcd0b71638017
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2023-08-18 09:55:09 -0300

    Add files via upload

---
+++ Estructura de Datos y Algoritmos/UNIDAD 2/PRACTICA/EJERCICIO 4/claseNodo.py
@@ -0,0 +1,19 @@
+class Nodo: 
+    __dato = int
+    __siguiente = None
+
+    def __init__(self, dato):
+        self.__dato = dato
+        self.__siguiente = None
+
+    def getDato(self):
+        return self.__dato
+    
+    def getSiguiente(self):
+        return self.__siguiente
+    
+    def setDato(self, dato):
+        self.__dato = dato
+
+    def setSiguiente(self, siguiente):
+        self.__siguiente = siguiente
