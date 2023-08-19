d8b14ab42f65990fd3120e465e7d21dfee2711c5
BaltasarOrtiz <103899892+BaltasarOrtiz@users.noreply.github.com>
2023-08-18 21:02:58 -0300

    Add files via upload

--- Estructura de Datos y Algoritmos/UNIDAD 3/PRACTICA_2/EJERCICIO 5/colaSecuencial.py
+++ Estructura de Datos y Algoritmos/UNIDAD 3/PRACTICA_2/EJERCICIO 5/colaSecuencial.py
@@ -4,8 +4,8 @@ class ColaSecuencial:
     __items = None
     __pr = None
     __ul = None
-    __cantidad = int
-    __dim = int
+    __cantidad: int
+    __dim: int
 
     def __init__(self, dimension):
         self.__items = np.empty(dimension, dtype=int)
@@ -24,22 +24,29 @@ class ColaSecuencial:
         if self.lleno():
             print("La cola esta llena")
         else:
-            self.__items[self.__ul]=dato
-            self.__ul=(self.__ul+1)%self.__dim
-            self.__cantidad+=1
+            self.__items[self.__ul]=dato #type:ignore
+            self.__ul=(self.__ul+1)%self.__dim #type:ignore
+            self.__cantidad+=1 
             print("Elemento insertado correctamente")
             
     
     def suprimir(self):
-        
+        x=0
+        if self.vacia():
+            print("La cola esta vacia")
+        else:
+            x=self.__items[self.__pr] #type:ignore
+            self.__pr=(self.__pr+1)%self.__dim #type:ignore
+            self.__cantidad-=1
+            return x
             
     
     def recorrer(self):
         i=self.__pr
         j=0
         while j<self.__cantidad:
-            print("Elemento: ", self.__items[i])
-            i=(i+1)%self.__dim
+            print("Elemento: ", self.__items[i]) #type:ignore
+            i=(i+1)%self.__dim #type:ignore
             j+=1
 
 
@@ -49,6 +56,9 @@ if __name__ == '__main__':
     cola.insertar(2)
     cola.insertar(3)
     print("\n\nMostrando Elementos")
+    cola.recorrer()
+    print("\n\nSuprimiendo Elemento: ", cola.suprimir())
+    print("Mostrando nuevamente elementos")
     cola.recorrer()
     
 
