d4f65fa3bd3144293e5b9dfb24ad5e414e66795d
BaltasarOrtiz <baltasarinfo2019@gmail.com>
2023-09-28 20:34:27 -0300

    Update monticulo.py

--- Estructura de Datos y Algoritmos/UNIDAD 4/PRACTICA_4/EJERCICIO_7/monticulo.py
+++ Estructura de Datos y Algoritmos/UNIDAD 4/PRACTICA_4/EJERCICIO_7/monticulo.py
@@ -1,14 +1,5 @@
-"""Ejercicio Nº7: Codifique un programa que simule 
-el funcionamiento de la lista de espera del quirófano de un hospital. 
-Cada vez que el quirófano esté desocupado, se operará al paciente de mayor urgencia, 
-dentro de esa lista de espera. Al ingresar un paciente al hospital, 
-además de solicitarle los datos personales, 
-se le asignará una prioridad relacionada con la gravedad de su caso."""
-
-#TAD Montículo Binario
-#Implementación: Montículo Binario con arreglo/numpy
-
 import numpy as np
+import os
 
 class Monticulo:
     __arreglo : np.ndarray
@@ -43,46 +34,114 @@ class Monticulo:
         self.__arreglo[pos1] = self.__arreglo[pos2]
         self.__arreglo[pos2] = aux
     
-    def insertar(self, dato):  #0:[-1]  1:[10]  2:[]  3:[]   4:[]   5:[]   lote: 10 20 30 40 50 60
+    def esHoja(self, posicion):
+        return posicion > self.__cantidad #Si la posición es mayor que la cantidad de elementos, es hoja
+
+    #Operaciones del TAD
+    def insertar(self, dato):  
         if self.lleno():
             print("Montículo lleno")
         else:
+            #Inserto el dato en la última posición
             self.__cantidad += 1
             self.__arreglo[self.__cantidad] = dato
             pos = self.__cantidad
 
-            padre= self.Padre(pos)
-            while self.__arreglo[pos] < self.__arreglo[padre]:
-                self.intercambiar(pos, padre)
+            #Se reordena el montículo
+            padre= self.Padre(pos) #Obtengo el padre
+
+            #Mientras el padre sea mayor que el hijo
+            while self.__arreglo[pos] < self.__arreglo[padre]: 
+                self.intercambiar(pos, padre) #Intercambio el padre con el hijo
+
+                #Actualizo las posiciones
                 pos = padre
                 padre = self.Padre(pos)
+    
+    def eliminar_minimo(self, padre):
+        hijoIzq = self.hijoIzquierdo(padre)
+        hijoDer = self.hijoDerecho(padre)
 
+        #Mientras no sea hoja y el padre sea mayor que alguno de sus hijos
+        while not self.esHoja(hijoDer) and (self.__arreglo[padre] > self.__arreglo[hijoIzq] or self.__arreglo[padre] > self.__arreglo[hijoDer]):
+            #Si el hijo izquierdo es menor que el hijo derecho
+            if self.__arreglo[hijoIzq] < self.__arreglo[hijoDer]:
+                self.intercambiar(padre, hijoIzq)
+                padre = hijoIzq
+            #Si el hijo derecho es menor que el hijo izquierdo
+            else:
+                self.intercambiar(padre, hijoDer)
+                padre = hijoDer
+            #Actualizo los hijos
+            hijoIzq = self.hijoIzquierdo(padre)
+            hijoDer = self.hijoDerecho(padre)
+
+    def eliminar(self): 
+        if self.vacio():
+            print("Montículo vacío")
+        else:
+            eliminado = self.__arreglo[1] #Guardo el dato a eliminar
+
+            self.__arreglo[1] = self.__arreglo[self.__cantidad] #Pongo el último elemento en la raíz 
+            self.__arreglo[self.__cantidad] = 0 #seteo el último elemento en None
+            self.__cantidad -= 1 #Decremento la cantidad de elementos
+
+            self.eliminar_minimo(1) #Reordeno el montículo
+            return eliminado
     
-    def mostrar(self):
-        print("Montículo: ", end = "")
-        for i in range(1, self.__cantidad + 1):
-            print(self.__arreglo[i], end = " ")
-        print()
-    
-    def mostrarArbol(self):
-        print("\n\nArbol: ")
-        for i in range(1, self.__cantidad + 1):
-            print("Nodo: ", self.__arreglo[i], end = " ")
-            if self.hijoIzquierdo(i) <= self.__cantidad:
-                print("Hijo Izquierdo: ", self.__arreglo[self.hijoIzquierdo(i)], end = " ")
-            if self.hijoDerecho(i) <= self.__cantidad:
-                print("Hijo Derecho: ", self.__arreglo[self.hijoDerecho(i)], end = " ")
-            print()
-        print()
-            
+    #Otras operaciones: Mostrar montículo     
+    def mostrarMonticulo(self, sangria=4):
+            if self.__cantidad == 0:
+                print("El montículo está vacío")
+                return
+
+            def mostrarMonticuloRec(posicion, cadena):
+                print(str(self.__arreglo[posicion]))
+                #Para el hijo derecho
+                hijoDer = self.hijoDerecho(posicion)
+                if hijoDer <= self.__cantidad:
+                    if self.hijoIzquierdo(posicion) <= self.__cantidad:
+                        print(cadena+ "├" + "─" * sangria, end="") 
+                    else:
+                        print(cadena+ "└" + "─" * sangria, end="")
+                    mostrarMonticuloRec(hijoDer, cadena + "│" + " " *sangria)
+                #Para el hijo izquierdo
+                hijoIzq = self.hijoIzquierdo(posicion)
+                if hijoIzq <= self.__cantidad:
+                    print(cadena+ "└" + "─" * sangria, end="")
+                    mostrarMonticuloRec(hijoIzq, cadena + " " *sangria)
+
+            mostrarMonticuloRec(1, "")
+        
 if __name__ == '__main__':
+    os.system("cls")
     m = Monticulo(10)
-    m.insertar(10)
-    m.insertar(20)
-    m.insertar(30)
-    m.insertar(40)
-    m.insertar(50)
-    m.insertar(60)
-    m.insertar(70)
-    m.mostrar()
-    m.mostrarArbol()
+    m.insertar(1)
+    m.insertar(2)
+    m.insertar(3)
+    m.insertar(4)
+    m.insertar(5)
+    m.insertar(6)
+    m.insertar(7)
+
+    print("Quirofano Lista de Espera:\n\n")
+    print("#Contexto: Nivel de urgencia[1-10] siendo 1 el más urgente y 10 el menos urgente\n")
+    m.mostrarMonticulo()
+
+    bandera= True
+    monticulo_desocupado = 5
+
+    while bandera != False:
+        if m.vacio():
+            print("El Quirofano no tiene pacientes en espera")
+            bandera = False
+        else:
+            if monticulo_desocupado == 0:
+                print("El paciente de mayor urgencia se esta operando...")
+                eliminado = m.eliminar()
+                print("Quirofano saca de la lista al paciente con urgencia: ", eliminado)
+                m.mostrarMonticulo()
+                monticulo_desocupado = 5
+            else:
+                print("El Quirofano esta ocupado")
+                monticulo_desocupado = 0
